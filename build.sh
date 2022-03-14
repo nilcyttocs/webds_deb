0ef473397b2f3fc1dd419fff6b9fc66032b62998
Scott Lin <scott.lin@tw.synaptics.com>
2022-03-14 12:22:52 +0800

    Remove archiving of repo at end of build

--- build.sh
+++ build.sh
@@ -7,5 +7,3 @@ cp wheelhouse/* pinormos-webds/webds-deb/var/spool/syna/jupyterlab_webds/.
 cp requirements.txt pinormos-webds/webds-deb/var/spool/syna/jupyterlab_webds/.
 pinormos-webds/gen-deb.sh
 rm -fr pinormos-webds/webds-deb/var/spool/syna/jupyterlab_webds/*
-rm -fr webds_deb.tar.gz
-tar -zcf webds_deb.tar.gz *
