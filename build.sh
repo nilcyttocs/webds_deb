033ffb8b8bec510f346ad6fd62db3cd07aaf8457
Scott Lin <scott.lin@tw.synaptics.com>
2022-05-16 23:45:13 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 
 rm -fr *.deb
 cp control pinormos-webds/webds-deb/DEBIAN/.
-cp wheelhouse/* pinormos-webds/webds-deb/var/spool/syna/jupyterlab_webds/.
-cp requirements.txt pinormos-webds/webds-deb/var/spool/syna/jupyterlab_webds/.
+cp wheelhouse/* pinormos-webds/webds-deb/var/spool/syna/webds/wheels/.
+cp requirements.txt pinormos-webds/webds-deb/var/spool/syna/webds/wheels/.
 pinormos-webds/gen-deb.sh
-rm -fr pinormos-webds/webds-deb/var/spool/syna/jupyterlab_webds/*
+rm -fr pinormos-webds/webds-deb/var/spool/syna/webds/wheels/*
