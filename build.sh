53bc476bc91f56a53d561dfc9b7d80ec4c7be6ff
Scott Lin <scott.lin@tw.synaptics.com>
2022-09-15 10:31:38 +0800

    Remove pinormos-webds/webds-deb/DEBIAN/control

--- build.sh
+++ build.sh
@@ -6,4 +6,5 @@ cp control pinormos-webds/webds-deb/DEBIAN/.
 cp wheelhouse/* pinormos-webds/webds-deb/var/spool/syna/webds/wheels/.
 cp requirements.txt pinormos-webds/webds-deb/var/spool/syna/webds/wheels/.
 pinormos-webds/gen-deb.sh
+rm -fr pinormos-webds/webds-deb/DEBIAN/control
 rm -fr pinormos-webds/webds-deb/var/spool/syna/webds/wheels/*
